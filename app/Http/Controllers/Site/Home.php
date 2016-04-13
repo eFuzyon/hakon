0b22891aba637f87b938fc2b6518b45ec789ad9c
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-04-13 18:42:28 -0300

    New controllers

---
+++ app/Http/Controllers/Site/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Home extends Controller
+{
+    public function Index(){
+    	return view('welcome');
+    }
+    public function Page(){
+    	return view('welcome');
+    }
+}
