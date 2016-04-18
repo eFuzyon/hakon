d0dd7c0b9f2e081c8f9d950cbbce6b43ccee4f41
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-04-18 19:24:21 -0300

    Changing bootstrap url

--- plugins/hakon/install/src/View/step-1.blade.php
+++ plugins/hakon/install/src/View/step-1.blade.php
@@ -4,7 +4,7 @@
         <title>Hakon</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link href="{{ asset("plugins/hakon/install/assets/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
 
         <style>
             html, body {
