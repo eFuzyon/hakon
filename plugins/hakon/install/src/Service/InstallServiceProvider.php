73a36cba295b0ada9b072e557c4931bb4a261820
Leandro Sanches <leandro.sanches@Mac-mini-de-Arca-3.local>
2016-04-21 14:39:06 -0300

    Adding events do service provider

--- plugins/hakon/install/src/Service/InstallServiceProvider.php
+++ plugins/hakon/install/src/Service/InstallServiceProvider.php
@@ -3,12 +3,25 @@
 namespace Plugin\Hakon\Install\Service;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
 
 class InstallServiceProvider extends ServiceProvider {
 
-	public function boot(){
+	public function boot(Dispatcher $events){
 
+		# Call parent
+		parent::boot($events);
+
+		# Views
 		$this->loadViewsFrom(__DIR__.'/../View', 'install');
+
+		# Events
+		$events->listen("App\Events\EventCoreTemplateMenuBefore", function(){
+            
+        });
+        $events->listen("App\Events\EventCoreTemplateMenuAfter", function(){
+            
+        });
 
 	}
 
