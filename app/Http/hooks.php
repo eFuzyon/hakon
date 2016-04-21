020a07c6fb33182f74e7eba86e3e221d5e15c057
Leandro Sanches <leandro.sanches@Mac-mini-de-Arca-3.local>
2016-04-21 14:38:47 -0300

    Changing hooks

--- app/Http/hooks.php
+++ app/Http/hooks.php
@@ -1,20 +1,31 @@
 <?php
 
+	use App\Events;
+	use App\Events\EventCoreTemplateMenuBefore;
+	use App\Events\EventCoreTemplateMenuAfter;
 	use Illuminate\Html\HtmlBuilder as Html;
 
 	Html::macro("Hook", function($name = null)
 	{
+
 		if ($name) :
-			Html::$name();
+
+			try {
+
+				Html::$name();
+
+			} catch(Exception $e){}
+
 		endif;
+
 	});
 
 	Html::macro("Core::Template::Menu::Before", function()
 	{
-		echo "Core::Template::Menu::Before";
+		Event::Fire(new EventCoreTemplateMenuBefore());
 	});
 
 	Html::macro("Core::Template::Menu::After", function()
 	{
-	    echo "Core::Template::Menu::After";
+	    Event::Fire(new EventCoreTemplateMenuAfter());
 	});
