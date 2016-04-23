f2080d64ce99cd514c248a0ec819361c817b12f7
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-04-23 17:24:42 -0300

    Updating middlewares on kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,11 +25,9 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\InstallMiddleware::class,
-            \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
         ],
 
         'api' => [
