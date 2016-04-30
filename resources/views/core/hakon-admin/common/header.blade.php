c2b27031e4b54c0a8084772cb44b8754fa8a498f
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-04-29 23:31:49 -0300

    Splitting views

---
+++ resources/views/core/hakon-admin/common/header.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hakon</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            a {
+                text-decoration: none;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title, .title a {
+                color: gray;
+                font-size: 96px;
+            }
+
+            .title a:hover {
+                color: black;
+            }
+
+            .subtitle, .subtitle a {
+                color: gray;
+                font-weight: bold;
+            }
+
+            .subtitle a:hover {
+                color: black;
+            }
+        </style>
+    </head>
