128e901a756a56d3719c2a1f0cd43eedd40fb7ec
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-01 22:51:54 -0300

    Changing hooks

--- app/Http/hooks.php
+++ app/Http/hooks.php
@@ -1,8 +1,6 @@
 <?php
 
 	use App\Events;
-	use App\Events\EventCoreTemplateMenuBefore;
-	use App\Events\EventCoreTemplateMenuAfter;
 	use Collective\Html\HtmlBuilder as Html;
 
 	Html::macro("Hook", function($name = null, $param = null)
@@ -22,14 +20,14 @@
 
 	Html::macro("Core::Admin::Template::Menu", function($param = null)
 	{
-		Event::Fire(new EventCoreTemplateMenuBefore($param));
-		Event::Fire(new EventCoreTemplateMenu($param));
-		Event::Fire(new EventCoreTemplateMenuAfter($param));
+		Event::Fire(new Events\EventCoreTemplateMenuBefore($param));
+		Event::Fire(new Events\EventCoreTemplateMenu($param));
+		Event::Fire(new Events\EventCoreTemplateMenuAfter($param));
 	});
 
 	Html::macro("Core::Admin::Template::Menu::Item", function($param = null)
 	{
-		Event::Fire(new EventCoreTemplateMenuItemBefore($param));
-		Event::Fire(new EventCoreTemplateMenuItem($param));
-		Event::Fire(new EventCoreTemplateMenuItemAfter($param));
+		Event::Fire(new Events\EventCoreTemplateMenuItemBefore($param));
+		Event::Fire(new Events\EventCoreTemplateMenuItem($param));
+		Event::Fire(new Events\EventCoreTemplateMenuItemAfter($param));
 	});
